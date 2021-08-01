839fc4056a2fc8594b2077027a2992ea80c67bee
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-08-01 07:41:46 -0400

    Remove unnecessary console.log

--- client/src/utils/errorHandler.js
+++ client/src/utils/errorHandler.js
@@ -1,8 +1,6 @@
 export default function errorHandler(error) {
   if (error.response) {
     console.log(error.response.data);
-    console.log(error.response.status);
-    console.log(error.response.headers);
   } else if (error.request) {
     console.log(error.request);
   } else {
